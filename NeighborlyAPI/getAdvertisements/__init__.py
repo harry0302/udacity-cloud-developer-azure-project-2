ba061dac675aab7104b5f130dbd3db50ec9aeca2
Luka <luka@192.168.2.20>
2024-09-14 13:23:23 +0700

    first commit

---
+++ NeighborlyAPI/getAdvertisements/__init__.py
@@ -0,0 +1,24 @@
+import os
+import azure.functions as func
+import pymongo
+import json
+from bson.json_util import dumps
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    try:
+        url = os.environ["MyDbConnection"]
+        client = pymongo.MongoClient(url)
+        database = client['lab2db']
+        collection = database['advertisements']
+
+
+        result = collection.find({})
+        result = dumps(result)
+
+        return func.HttpResponse(result, mimetype="application/json", charset='utf-8')
+    except:
+        print("could not connect to mongodb")
+        return func.HttpResponse("could not connect to mongodb",
+                                 status_code=400)
+
